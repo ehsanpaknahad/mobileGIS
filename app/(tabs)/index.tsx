3632903d556469d7e9f2f0aa4a56e67f25432414
ehsanpaknahad <packnahad@yahoo.com>
2024-05-18 09:45:23 +0330

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Image, StyleSheet, Platform, View ,Text} from 'react-native';
+  
+
+export default function HomeScreen() {
+  return (
+     <View>
+         <Text> our react native blank native canvas </Text>
+     </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
