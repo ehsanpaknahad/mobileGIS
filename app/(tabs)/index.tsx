3c51e87293936ce96b24685887bbeebe8919c6da
ehsanpaknahad <packnahad@yahoo.com>
2024-05-18 14:38:29 +0330

    delete extra files

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,42 @@
-import { Image, StyleSheet, Platform, View ,Text} from 'react-native';
-  
+ 
 
-export default function HomeScreen() {
+
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+const HomeScreen = () => {
   return (
-     <View>
-         <Text> our react native blank native canvas </Text>
-     </View>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          title="Marker Title"
+          description="Marker Description"
+        />
+      </MapView>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
 });
+
+export default HomeScreen;
